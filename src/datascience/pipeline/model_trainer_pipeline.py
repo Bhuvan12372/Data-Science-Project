49fd3662a5ea009920e1612d1589c9a1a8963ae5
Bhuvan <bhuvankatakam@gmail.com>
2025-02-11 11:32:22 +0530

    Small change in pipeline

--- src/datascience/pipeline/model_trainer_pipeline.py
+++ src/datascience/pipeline/model_trainer_pipeline.py
@@ -5,7 +5,7 @@ class ModelTraineronPipeline:
         pass
 
 
-    def model_training_pipeline():
+    def model_training_pipeline(self):
         config = ConfigurationManager()
         model_trainer_config = config.get_model_trainer_config()
         model_trainer = ModelTrainer(model_trainer_config)
